7b1aa88a9bdd262512c50822ee0705df53d9258c
hhp227 <hong227@naver.com>
2021-11-24 23:30:48 +0900

    2021.11.24 Commit

--- app/src/main/java/com/programmers/kmooc/KmoocApplication.kt
+++
@@ -1,8 +0,0 @@
-package com.programmers.kmooc
-
-import android.app.Application
-import com.programmers.kmooc.repositories.KmoocRepository
-
-class KmoocApplication : Application() {
-    val kmoocRepository = KmoocRepository()
-}
--- app/src/main/java/com/programmers/kmooc/utils/ViewUtil.kt
+++
@@ -1,7 +0,0 @@
-package com.programmers.kmooc.utils
-
-import android.view.View
-
-fun Boolean.toVisibility(): Int {
-    return if (this) View.VISIBLE else View.GONE
-}
