0a9a70700551e6218b9fde14a540ee24fc7165aa
hhp227 <hong227@naver.com>
2021-11-24 23:03:37 +0900

    2021.11.24 Commit

---
+++ DateMate/DateMateApp.swift
@@ -0,0 +1,17 @@
+//
+//  DateMateApp.swift
+//  DateMate
+//
+//  Created by 홍희표 on 2021/11/24.
+//
+
+import SwiftUI
+
+@main
+struct DateMateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
