8abf68fa6daa853027f2f334ab19e30000489b8a
hhp227 <hong227@naver.com>
2021-11-27 23:22:20 +0900

    2021.11.27 Commit

---
+++ DateMate/LoginView.swift
@@ -0,0 +1,20 @@
+//
+//  LoginView.swift
+//  DateMate
+//
+//  Created by 홍희표 on 2021/11/27.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
+}
