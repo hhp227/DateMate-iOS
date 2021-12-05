3b51bf4f39e0da2e3bfa7a863f0e97e327458d1e
hhp227 <hong227@naver.com>
2021-12-05 23:59:26 +0900

    2021.12.05 Commit

--- DateMate/SignUpViewModel.swift
+++ DateMate/SignUpViewModel.swift
@@ -9,17 +9,12 @@ import Foundation
 import Firebase
 
 class SignUpViewModel: ObservableObject {
-    @Published var loggedIn = false
-    
     let auth = Auth.auth()
     
     func signUp(email: String, password: String) {
         auth.createUser(withEmail: email, password: password) { result, error in
             guard result != nil, error == nil else {
                 return
-            }
-            DispatchQueue.main.async {
-                self.loggedIn = true
             }
         }
     }
