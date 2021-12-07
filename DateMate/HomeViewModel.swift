e6f051ba952e6c9f5dabc4ae6734d722c82e05bd
hhp227 <hong227@naver.com>
2021-12-07 22:55:38 +0900

    2021.12.07 Commit

---
+++ DateMate/HomeViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  HomeViewModel.swift
+//  DateMate
+//
+//  Created by 홍희표 on 2021/12/07.
+//
+
+import Foundation
+
+class HomeViewModel: ObservableObject {
+    
+}
---
+++ DateMate/RoungeViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  RoungeViewModel.swift
+//  DateMate
+//
+//  Created by 홍희표 on 2021/12/07.
+//
+
+import Foundation
+
+class RoungeViewModel: ObservableObject {
+    
+}
