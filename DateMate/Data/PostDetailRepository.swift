bbadd8e54775d8f0951739740582e322ae177217
hhp227 <hong227@naver.com>
2022-01-09 23:17:47 +0900

    2022.01.09 Commit

--- DateMate/Data/PostDetailRepository.swift
+++ DateMate/Data/PostDetailRepository.swift
@@ -63,7 +63,6 @@ class PostDetailRepository {
     func removePost(_ key: String) {
         guard let user = Auth.auth().currentUser else { fatalError() }
         userPostRef.child(user.uid).getData { error, dataSnapshot in
-            dataSnapshot.childSnapshot(forPath: key)
             print("test: \(dataSnapshot.childSnapshot(forPath: key))")
         }
     }
